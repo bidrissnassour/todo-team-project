1eecb76d10ace05cfea10f13d08d90ea13784e0d
djamnonedjoldebnemazaare <mazaaremazaare@gmail.com>
2026-06-03 02:01:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+  HEAD
 const addBtn = document.getElementById('addBtn');
 const taskInput = document.getElementById('taskInput');
 const taskList = document.getElementById('taskList');
